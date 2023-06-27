bd16a6180d819ded4b274001396c0769b243ab28
aaron tanjaya <aaront.tjy@gmail.com>
2023-06-27 12:13:48 +0700

    feat: add price to menu items

--- src/components/molecules/CardMenuItem/index.tsx
+++ src/components/molecules/CardMenuItem/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Button, Image } from "@/components/atoms";
 import Card from "../Card";
 import { CardMenuItemProps } from "../interface";
-import { colors } from "@/styles/variables";
+import { formatAmount, getNumberOrDefault } from "@/helpers/accounting";
 import { PlusOutlined } from "@ant-design/icons";
 import styles from "./styles.module.scss";
 
 const CardMenuItem: React.FC<CardMenuItemProps> = ({ item, onItemClick }) => {
+  const price = item.discountedPriceV2
+    ? getNumberOrDefault(item.discountedPriceV2.amountInMinor)
+    : getNumberOrDefault(item.priceV2.amountInMinor);
   return (
     <Card className={styles.container}>
       <Image
@@ -15,6 +18,29 @@ const CardMenuItem: React.FC<CardMenuItemProps> = ({ item, onItemClick }) => {
       />
       <div className={styles.content}>
         <h3>{item.name}</h3>
+        {item.discountedPriceV2 &&
+        item.discountedPriceV2.amountInMinor != item.priceV2.amountInMinor ? (
+          <div className={styles.price__container}>
+            <p className={styles.discount}>
+              {formatAmount(
+                getNumberOrDefault(item.priceV2.amountInMinor) / 100
+              )}
+            </p>
+            <p>
+              {formatAmount(
+                getNumberOrDefault(item.discountedPriceV2.amountInMinor) / 100
+              )}
+            </p>
+          </div>
+        ) : (
+          <div className={styles.price__container}>
+            <p>
+              {formatAmount(
+                getNumberOrDefault(item.priceV2.amountInMinor) / 100
+              )}
+            </p>
+          </div>
+        )}
         <Button
           className={styles.button}
           type="primary"
