746edeead6d447a7fb81e6ba319a1d2e7928bdbf
aaron tanjaya <aaront.tjy@gmail.com>
2023-06-27 13:23:50 +0700

    refactor: enhance card transaction

--- src/components/molecules/CardTransaction/index.tsx
+++ src/components/molecules/CardTransaction/index.tsx
@@ -3,6 +3,7 @@ import { Button, Image } from "@/components/atoms";
 import Card from "../Card";
 import { CardTransactionProps } from "../interface";
 import { colors } from "@/styles/variables";
+import { formatAmount, getNumberOrDefault } from "@/helpers/accounting";
 import { DownOutlined, UpOutlined } from "@ant-design/icons";
 import styles from "./styles.module.scss";
 
@@ -11,42 +12,54 @@ const CardTransaction: React.FC<CardTransactionProps> = ({ item }) => {
 
   const footer = show ? (
     <Button
+      className={styles.button}
       onClick={() => setShow(false)}
       type="text"
-      icon={<UpOutlined color={colors.redPrimary} />}
-    />
+    >
+      <UpOutlined color={colors.redPrimary} />
+    </Button>
   ) : (
-    <Button
-      onClick={() => setShow(true)}
-      type="text"
-      icon={<DownOutlined color={colors.redPrimary} />}
-    />
+    <Button className={styles.button} onClick={() => setShow(true)} type="text">
+      <DownOutlined color={colors.redPrimary} />
+    </Button>
   );
   return (
     <Card className={styles.container}>
-      <Image
-        imgClassName={styles.image}
-        src={
-          item.items[0] && item.items[0].item.thumbImages[0]
-            ? item.items[0].item.thumbImages[0]
-            : ""
-        }
-      />
-      <div className={styles.content}>
-        <h4>{item.items[0].item.name}</h4>
-        <p>{item.totalPrice}</p>
-        <p>{item.date}</p>
-        {show
-          ? item.items.map((item) => (
-              <>
-                <p>{item.item.name}</p>
-                <p>{item.quantity}</p>
-                <p>{item.price}</p>
-              </>
-            ))
-          : null}
-        {item.items.length > 0 ? footer : null}
+      <div className={styles.content__container}>
+        <Image
+          imgClassName={styles.image}
+          src={
+            item.items[0] && item.items[0].item.thumbImages[0]
+              ? item.items[0].item.thumbImages[0]
+              : ""
+          }
+        />
+        <div className={styles.content}>
+          <h4 className={styles.item__name}>{item.items[0].item.name}</h4>
+          <p>{`Subtotal Rp. ${formatAmount(
+            getNumberOrDefault(item.totalPrice)
+          )}`}</p>
+
+          {show ? (
+            <>
+              <h4 className={styles.text__breakdown}>Order Breakdown</h4>
+              {item.items.map((item) => (
+                <div className={styles.item__container}>
+                  <div className={styles.item__detail}>
+                    <p className={styles.item__name}>{item.item.name}</p>
+                    <p
+                      className={styles.item__quantity}
+                    >{`${item.quantity}x`}</p>
+                  </div>
+                  <p>{`Rp. ${formatAmount(getNumberOrDefault(item.price))}`}</p>
+                </div>
+              ))}
+            </>
+          ) : null}
+          <p className={styles.date}>{`Ordered at ${item.date}`}</p>
+        </div>
       </div>
+      {item.items.length > 1 ? footer : null}
     </Card>
   );
 };
--- src/components/pages/Transactions/index.tsx
+++ src/components/pages/Transactions/index.tsx
@@ -3,12 +3,14 @@ import { TransactionsProps } from "../interfaces";
 import { Order } from "@/components/entity";
 import { Card, CardTransaction } from "@/components/molecules";
 import useFetch from "@/hooks/useFetch";
+import styles from "./styles.module.scss";
 const Transactions: React.FC<TransactionsProps> = () => {
   const { data, loading, error, errorMessage } = useFetch<Order[]>(
     `${process.env.REACT_APP_URL}/api/order`
   );
   return (
-    <div>
+    <div className={styles.container}>
+      <h3 className={styles.page__title}>Transaction History</h3>
       {data ? data.map((item) => <CardTransaction item={item} />) : null}
     </div>
   );
